0bd6f577acf9027d18c2d2d05a57a9d940eb3c71
JonnyRoller21 <71149028+JonnyRoller21@users.noreply.github.com>
2021-11-14 13:33:57 +0300

    Update dumpyara.sh

--- dumpyara.sh
+++ dumpyara.sh
@@ -26,7 +26,7 @@ else
     [[ -e "$URL" ]] || { echo "Invalid Input" && exit 1; }
 fi
 
-ORG=EXOdev-698 #your GitHub org name
+ORG=JonnyRoller21 #your GitHub org name
 FILE=$(echo ${URL##*/} | inline-detox)
 EXTENSION=$(echo ${URL##*.} | inline-detox)
 UNZIP_DIR=${FILE/.$EXTENSION/}
